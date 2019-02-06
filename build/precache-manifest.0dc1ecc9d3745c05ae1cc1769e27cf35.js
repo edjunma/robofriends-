095a7c70999e8228a12638cab9876c691b59775b
edjunma <junwenma@buffalo.edu>
2019-02-06 01:48:38 -0500

    Add deploy build folderv

---
+++ build/precache-manifest.0dc1ecc9d3745c05ae1cc1769e27cf35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7559cdd25c029d545490",
+    "url": "/static/js/main.7559cdd2.chunk.js"
+  },
+  {
+    "revision": "99109a5fb9182cc41ec6",
+    "url": "/static/js/1.99109a5f.chunk.js"
+  },
+  {
+    "revision": "7559cdd25c029d545490",
+    "url": "/static/css/main.17e78d90.chunk.css"
+  },
+  {
+    "revision": "99109a5fb9182cc41ec6",
+    "url": "/static/css/1.34703468.chunk.css"
+  },
+  {
+    "revision": "f0d469a56f63dcb6d715347cb709be4a",
+    "url": "/index.html"
+  }
+];
